778fd8697acf0524f7aabf029f528bb051ffddec
Tyler <hi@tylerfinck.com>
2020-06-09 15:33:49 -0400

    meta updates for testing fixes

--- Sources/build.sh
+++ Sources/build.sh
@@ -5,6 +5,30 @@ set -e
 
 mkdir -p ./fonts ./fonts/static/ttf ./fonts/static/otf ./fonts/variable
 
+
+echo "Generating VFs"
+fontmake -g Sources/epilogue.glyphs -o variable --output-path ./fonts/variable/Epilogue[wght].ttf
+fontmake -g Sources/epilogue_italic.glyphs -o variable --output-path ./fonts/variable/Epilogue-Italic[wght].ttf
+
+
+
+
+echo "Post processing VFs"
+for ttf in ./fonts/variable/*.ttf
+do
+	gftools fix-dsig --autofix $ttf;
+	gftools fix-nonhinting $ttf "$ttf.fix";
+	mv "$ttf.fix" $ttf;
+	gftools fix-unwanted-tables --tables MVAR $ttf
+	gftools fix-vf-meta $ttf;
+	mv "$ttf.fix" $ttf;
+
+done
+
+
+rm ./fonts/variable/*gasp*
+
+
 echo "Generating Static fonts"
 fontmake -g Sources/epilogue.glyphs -i -o ttf --output-dir ./fonts/static/ttf/
 fontmake -g Sources/epilogue_italic.glyphs -i -o ttf --output-dir ./fonts/static/ttf/
@@ -12,12 +36,6 @@ fontmake -g Sources/epilogue_italic.glyphs -i -o ttf --output-dir ./fonts/static
 fontmake -g Sources/epilogue.glyphs -i -o otf --output-dir ./fonts/static/otf/
 fontmake -g Sources/epilogue_italic.glyphs -i -o otf --output-dir ./fonts/static/otf/
 
-echo "Generating VFs"
-fontmake -g Sources/epilogue.glyphs -o variable --output-path ./fonts/variable/Epilogue[wght].ttf
-fontmake -g Sources/epilogue_italic.glyphs -o variable --output-path ./fonts/variable/Epilogue-Italic[wght].ttf
-
-
-rm -rf master_ufo/ instance_ufo/
 
 
 echo "Post processing TTFs"
@@ -65,25 +83,8 @@ for woff in $woffs; do
 done
 
 
-echo "Post processing VFs"
-vfs=$(ls ./fonts/variable/*.ttf)
-for vf in $vfs
-do
-	gftools fix-dsig --autofix $vf;
-	gftools fix-nonhinting $vf "$vf.fix";
-	mv "$vf.fix" $vf;
-	gftools fix-unwanted-tables --tables MVAR $vf
 
-done
-
-
-gftools fix-vf-meta $vfs;
-for vf in $vfs
-do
-mv "$vf.fix" $vf;
-done
-rm ./fonts/variable/*gasp*
-
+rm -rf master_ufo/ instance_ufo/
 
 
 
